daeea91be25391581a46cd8397784259b3767640
Preeti <singhp1992@gmail.com>
2018-05-07 14:35:45 +0200

    added db.ts, jwt.ts, manipulted index.ts to import db.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata'
 import { createKoaServer } from "routing-controllers"
 import Controller from "./controller"
+import setupDb from './db'
 
 const port = process.env.PORT || 4000
 
@@ -8,4 +9,8 @@ const app = createKoaServer({
     controllers: [Controller]
 })
 
-app.listen(port, () => console.log(`Listening on port ${port}`))
+setupDb()
+    .then(_ =>
+        app.listen(4000, () => console.log('Listening on port 4000'))
+    )
+    .catch(err => console.error(err))
